80a76e2a0e9310aee48cf0084d117d82a6bd055f
cassius <cassiusbessa@gmail.com>
2024-11-07 11:10:56 -0300

    normalize main.c

--- main.c
+++ main.c
@@ -1,46 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cassius <cassius@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 11:07:38 by cassius           #+#    #+#             */
+/*   Updated: 2024/11/07 11:10:13 by cassius          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
+static int	create_philo_threads(t_philosopher *philosophers, t_table *table);
 
 int	main(int argc, char **argv)
 {
-	t_arguments	arguments;
-	t_table					table;
-	t_philosopher		*philosophers;
-	int							i;
+	t_arguments		arguments;
+	t_table			table;
+	t_philosopher	*philosophers;
+	int				i;
 
 	arguments = parser(argc, argv);
-	print_args(arguments);
 	if (arguments.error)
 		return (1);
 	init_table(&table, arguments);
-	philosophers = malloc(sizeof(t_philosopher) * arguments.number_of_philosophers);
+	philosophers = malloc(sizeof(t_philosopher)
+			* arguments.number_of_philosophers);
 	if (!philosophers)
-	{
-		destroy_table(&table);
-		return (1);
-	}
+		return (destroy_table(&table), 1);
 	init_philosophers(philosophers, &table);
-	i = 0;
 	gettimeofday(&table.tv_start, NULL);
+	if (create_philo_threads(philosophers, &table))
+		return (1);
+	monitor(&table, philosophers);
+	i = 0;
 	while (i < arguments.number_of_philosophers)
+		pthread_join(philosophers[i++].thread, NULL);
+	destroy_table(&table);
+	free(philosophers);
+}
+
+static int	create_philo_threads(t_philosopher *philosophers, t_table *table)
+{
+	int	i;
+
+	i = 0;
+	while (i < table->args.number_of_philosophers)
 	{
-		if (pthread_create(&philosophers[i].thread, NULL, philosopher_routine, &philosophers[i]))
+		if (pthread_create(&philosophers[i].thread, NULL, philosopher_routine,
+				&philosophers[i]))
 		{
 			while (--i >= 0)
-      			pthread_join(philosophers[i].thread, NULL);
-			destroy_table(&table);
+				pthread_join(philosophers[i].thread, NULL);
+			destroy_table(table);
 			free(philosophers);
 			return (1);
 		}
 		i++;
 	}
-	monitor(&table, philosophers);
-	i = 0;
-	while (i < arguments.number_of_philosophers)
-	{
-		pthread_join(philosophers[i].thread, NULL);
-		i++;
-	}
-	destroy_table(&table);
-	free(philosophers);
+	return (0);
 }
